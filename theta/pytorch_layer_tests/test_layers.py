3aa5ecb7c58f81951da12959a85b87e9078a3cba
Taylor Childers <taylorchilders@gmail.com>
2019-09-18 09:06:53 -0500

    adding merge script and adding mpi

--- theta/pytorch_layer_tests/test_layers.py
+++ theta/pytorch_layer_tests/test_layers.py
@@ -4,18 +4,27 @@ logger = logging.getLogger(__name__)
 import torch
 import pandas as pd
 import numpy as np
+from mpi4py import MPI
 from ptflops import get_model_complexity_info
 
 
 def main():
    ''' simple starter program that can be copied for use when starting a new script. '''
-   logging_format = '%(asctime)s %(levelname)s:%(name)s:%(message)s'
+
+   comm = MPI.COMM_WORLD
+   rank = comm.Get_rank()
+   nranks = comm.Get_size()
+
+   logging_format = '%(asctime)s %(levelname)s:' + ('%05d' % rank) + ':%(name)s:%(message)s'
    logging_datefmt = '%Y-%m-%d %H:%M:%S'
-   logging_level = logging.INFO
+   if rank == 0:
+      logging_level = logging.INFO
+   else:
+      logging_level = logging.WARNING
    
    parser = argparse.ArgumentParser(description='')
-   # parser.add_argument('-i','--input',dest='input',help='input',required=True)
-   parser.add_argument('-o','--output_filename',help='output jasn filename',default='test_layers.json')
+   parser.add_argument('-i','--input_filename',help='input filename of previous data points measured. These will be skipped in the new run.')
+   parser.add_argument('-o','--output_filename',help='output json filename, should include "%%05d" somewhere in the name for the rank ID. ',default='test_layers_%05d.json')
    parser.add_argument('--debug', dest='debug', default=False, action='store_true', help="Set Logger to DEBUG")
    parser.add_argument('--error', dest='error', default=False, action='store_true', help="Set Logger to ERROR")
    parser.add_argument('--warning', dest='warning', default=False, action='store_true', help="Set Logger to ERROR")
@@ -34,100 +43,111 @@ def main():
                        datefmt=logging_datefmt,
                        filename=args.logfilename)
    
-   # # Conv2d tests
-   # batch_size = 10
-   # image_size = (128,128)
-   # in_channels = 3
-   # out_channels = 16
+   # parse the rank into the output filename
+   output_filename = args.output_filename % rank
+   logger.info('input filename:  %s',args.input_filename)
+   logger.info('output filename: %s',output_filename)
+   
+   combinations = get_combo_list()
 
-   # layer = torch.nn.Conv2d(in_channels,out_channels,(3,3),stride=1,padding=1,bias=False)
-   # opt = torch.optim.SGD(layer.parameters(),lr=0.1)
-   # lossfunc = torch.nn.BCELoss()
-   # activation = torch.nn.Sigmoid()
+   previous_data = init_data(args.input_filename)
 
-   # opt.zero_grad()
+   logger.info('combinations prior to dedupe: %s',len(combinations))
+   combinations = remove_duplicates(previous_data,combinations)
+   logger.info('combinations after dedupe: %s',len(combinations))
 
-   # inputs = torch.randn((batch_size,in_channels) + image_size,dtype=torch.float32)
-   # targets = torch.randn((batch_size,out_channels) + image_size,dtype=torch.float32)
-   # targets = torch.abs(targets)
+   new_data = []
+
+   for i in range(rank,len(combinations),nranks):
+      combination = combinations[i]
+      logger.info('running %s',combination)
+      test_data = time_cnn_layer(in_channels=combination['in_channels'],
+                                 out_channels=combination['out_channels'],
+                                 kernel_size=combination['kernel_size'],
+                                 batch_size=combination['batch_size'],
+                                 image_size=combination['image_size'])
+      new_data.append(test_data)
+
+      json.dump(new_data,open(output_filename,'w'))
 
 
-
+def get_combo_list():
    kernel_sizes = [(2,2),(3,3),(4,4),(5,5)]
    in_channels = [1,3,4,8,16,32,64,128,256,512,1024]
    out_channels = in_channels.copy()
    image_sizes = [(32,32),(64,32),(64,64),(128,64),(128,128), (256,128),(256,256),(512,256),(512,512),(1024,512),(1024,1024)]
    batch_sizes = [16,32,64,128,256,512]
 
+   # make list of combinations
+   combinations = []
+   for batch_size in batch_sizes:
+      for in_channel in in_channels:
+         for out_channel in out_channels:
+            for image_size in image_sizes:
+               for kernel_size in kernel_sizes:
+                  combination = {}
+                  combination['batch_size'] = batch_size
+                  combination['in_channels'] = in_channel
+                  combination['out_channels'] = out_channel
+                  combination['image_size'] = image_size
+                  combination['kernel_size'] = kernel_size
+                  combinations.append(combination)
+   return combinations
+
+
+def get_header(kernel_dim,image_dim):
    string = ''
    string += '%10s \t' % 'class'
    string += '%10s \t' % 'flops'
    string += '%10s \t' % 'in_channels'
    string += '%10s \t' % 'out_channels'
-   string += '%12s '   % 'kernel_size' + '\t' * len(kernel_sizes[0])
+   string += '%12s '   % 'kernel_size' + '\t' * kernel_dim
    string += '%10s \t' % 'padding'
    string += '%10s \t' % 'stride'
    string += '%10s \t' % 'bias'
    string += '%10s \t' % 'batch_size'
-   string += '%12s '   % 'image_size' + '\t' * len(image_sizes[0])
+   string += '%12s '   % 'image_size' + '\t' * image_dim
    string += '%10s \t' % 'precision'
    string += '%10s \t' % 'min'
    string += '%10s \t' % 'mean'
    string += '%10s \t' % 'sigma'
    string += '%10s \n' % 'max'
    sys.stderr.write(string)
+   return string
 
-   if os.path.exists(args.output_filename):
-      data = json.load(open(args.output_filename))
+
+def init_data(filename=None):
+   if filename is not None and os.path.exists(filename):
+      data = json.load(open(filename))
       logger.info('found data with %s entries',len(data))
    else:
       data = []
 
-   for batch_size in batch_sizes:
-      for in_channel in in_channels:
-         for out_channel in out_channels:
-            for image_size in image_sizes:
-               for kernel_size in kernel_sizes:
+   return data
 
-                  done_already = False
-                  for entry in data:
-                     if (entry['batch_size']    == batch_size and
-                         entry['in_channels']   == in_channel and
-                         entry['out_channels']  == out_channel and
-                         tuple(entry['image_size'])    == image_size and
-                         tuple(entry['kernel_size'])   == kernel_size):
-                        done_already = True
+
+def remove_duplicates(data,combinations):
+
+   deduped = []
+   counter = 0
+   for combination in combinations:
+      dupe = False
+      for entry in data:
+         if combination['batch_size'] == entry['batch_size']:
+            if combination['in_channels'] == entry['in_channels']:
+               if combination['out_channels'] == entry['out_channels']:
+                  if combination['image_size'] == tuple(entry['image_size']):
+                     if combination['kernel_size'] == tuple(entry['kernel_size']):
+                        dupe = True
+                        counter += 1
                         break
-
-                  if done_already:
-                     break
-
-                  test_data = time_cnn_layer(in_channels=in_channel,
-                                             out_channels=out_channel,
-                                             kernel_size=kernel_size,
-                                             batch_size=batch_size,
-                                             image_size=image_size)
-                  data.append(test_data)
-
-                  json.dump(data,open(args.output_filename,'w'))
+      if not dupe:
+         deduped.append(combination)
+   print(counter)
+   return deduped
 
 
-   #time_cnn_layer()
-   #min,mean,sigma,max,pred = timer(layer,100,inputs)
-   #logger.info('fwd: min = %6.3f  mean = %6.3f +/- %6.3f  max = %6.3f',min,mean,sigma,max)
-
-
-   # min,mean,sigma,max,pred = timer(activation,100,pred)
-   # logger.info('act: min = %6.3f  mean = %6.3f +/- %6.3f  max = %6.3f',min,mean,sigma,max)
-
-   # min,mean,sigma,max,loss = timer(lossfunc,100,pred,targets)
-   # logger.info('loss: min = %6.3f  mean = %6.3f +/- %6.3f  max = %6.3f',min,mean,sigma,max)
-
-   # min,mean,sigma,max,loss = timer(loss.backward,100,layer.parameters(),True)
-   # logger.info('loss: min = %6.3f  mean = %6.3f +/- %6.3f  max = %6.3f',min,mean,sigma,max)
-
-
-def time_cnn_layer(layer_class=torch.nn.Conv2d ,in_channels=3,out_channels=16,kernel_size=(3,3),padding=1,stride=1,bias=False,batch_size=100,timed_tests=100,image_size=(128,128),precision='float32'):
+def time_cnn_layer(layer_class=torch.nn.Conv2d,in_channels=3,out_channels=16,kernel_size=(3,3),padding=1,stride=1,bias=False,batch_size=100,timed_tests=100,image_size=(128,128),precision='float32'):
 
    layer = layer_class(in_channels,out_channels,kernel_size,stride=stride,bias=bias)
    if hasattr(layer,precision):
@@ -156,28 +176,27 @@ def time_cnn_layer(layer_class=torch.nn.Conv2d ,in_channels=3,out_channels=16,ke
       'max':max,
    }
 
-   string = ''
-   string += '%10s\t' % layer_class.__name__
-   string += '%10d\t' % flops
-   string += '%10d\t' % in_channels
-   string += '%10d\t' % out_channels
-   string += ''.join('%6d\t' % x for x in kernel_size)
-   string += '%10d\t' % padding
-   string += '%10d\t' % stride
-   string += '%10d\t' % bias
-   string += '%10d\t' % batch_size
-   string += ''.join('%6d\t' % x for x in image_size)
-   string += '%10s\t' % precision
-   string += '%10.6f\t' % min
-   string += '%10.6f\t' % mean
-   string += '%10.6f\t' % sigma
-   string += '%10.6f\n' % max
-   sys.stderr.write(string)
-   sys.stderr.flush()
+   # string = ''
+   # string += '%10s\t' % layer_class.__name__
+   # string += '%10d\t' % flops
+   # string += '%10d\t' % in_channels
+   # string += '%10d\t' % out_channels
+   # string += ''.join('%6d\t' % x for x in kernel_size)
+   # string += '%10d\t' % padding
+   # string += '%10d\t' % stride
+   # string += '%10d\t' % bias
+   # string += '%10d\t' % batch_size
+   # string += ''.join('%6d\t' % x for x in image_size)
+   # string += '%10s\t' % precision
+   # string += '%10.6f\t' % min
+   # string += '%10.6f\t' % mean
+   # string += '%10.6f\t' % sigma
+   # string += '%10.6f\n' % max
+   # sys.stderr.write(string)
+   # sys.stderr.flush()
    #logger.info('%s: min = %6.3f  mean = %6.3f +/- %6.3f  max = %6.3f',layer_class.__name__,min,mean,sigma,max)
 
    return test_data
-
 
 
 def timer(func,tests,*argv):
